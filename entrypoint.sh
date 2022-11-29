5afce33bdca76c0a3405b6da626e96de31cff66a
m-oka-system <m-oka-system@outlook.com>
2022-11-29 10:10:08 +0900

    [update]スタートアップ処理にdb:createを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ rm -f /workspace/tmp/pids/server.pid
 
 # 起動時にマイグレーションを実行する
 if [[ $RAILS_ENV = 'production' ]]; then
-  rails db:migrate
+  rails db:create db:migrate
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
